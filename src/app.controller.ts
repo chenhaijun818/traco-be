81f14fc1c43cee0aefc140e50dc228f26dfa8d9a
陈海俊 <www.353376352@qq.com>
2022-12-01 23:26:28 +0800

    demo: upload, download

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Body, UploadedFile, UseInterceptors, StreamableFile } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { FileInterceptor } from "@nestjs/platform-express";
+import * as sharp from 'sharp';
+import { createReadStream } from "fs";
+import { join } from "path";
+import * as process from "process";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  upload(@UploadedFile() file): any {
+    const image = sharp(file.buffer);
+    image.metadata().then(data => {
+      if (data.width > 300) {
+
+      }
+    })
+    return 'ok'
+  }
+
+  @Get('getFile')
+  getFile(): StreamableFile {
+    const file = createReadStream(join(process.cwd(), 'src/statics/rjjsbx.jpg'));
+    return new StreamableFile(file);
+  }
+
 }
