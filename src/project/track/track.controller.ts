df82d01a08444fc6e1daa3254829dee9ca38dba1
陈海俊 <353376352@qq.com>
2024-01-02 02:49:38 +0800

    新增track时初始化visible字段

--- src/project/track/track.controller.ts
+++ src/project/track/track.controller.ts
@@ -2,26 +2,19 @@ import {Body, Controller, Get, Post, Query} from '@nestjs/common';
 import {TrackDocument} from "../models/track";
 import {Model} from "mongoose";
 import {InjectModel} from "@nestjs/mongoose";
-import {ProjectDocument} from "../models/project";
 
 @Controller('project/track')
 export class TrackController {
-    constructor(@InjectModel("Track") private track: Model<TrackDocument>,
-                @InjectModel("Project") private project: Model<ProjectDocument>
-    ) {
+    constructor(@InjectModel("Track") private track: Model<TrackDocument>) {
     }
 
     @Post('add')
     async add(@Body() body) {
-        const project = await this.project.findById(body.pid);
-        console.log(project)
         const res = await this.track.create({
             name: body.name,
             pid: body.pid,
-            order: project.trackCount + 1,
-            affairCount: 1
+            visible: true
         });
-        project.update({trackCount: project.trackCount + 1})
         return {code: 200, data: res, message: 'success'}
     }
 
@@ -43,7 +36,6 @@ export class TrackController {
 
     @Get('tracks')
     async getTracks(@Query() query) {
-        console.log(query)
         const res = await this.track.find({pid: query.pid}).exec();
         return {code: 200, data: res, message: 'success'}
     }
