c8a494302aa9d1b7696acd8c34782ac7c4e836e8
陈海俊 <353376352@qq.com>
2024-01-03 03:22:14 +0800

    get affair:id

--- src/project/role/role.controller.ts
+++ src/project/role/role.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post, Query} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Query} from '@nestjs/common';
 import {InjectModel} from "@nestjs/mongoose";
 import {RoleDocument} from "../models/role";
 import {Model} from "mongoose";
@@ -37,6 +37,12 @@ export class RoleController {
     @Get('roles')
     async getRoles(@Query() query) {
         const res = await this.role.find({pid: query.pid}).exec();
+        return {code: 200, data: res, message: 'success'}
+    }
+
+    @Get(':id')
+    async getAffair(@Param() param) {
+        const res = await this.role.findById(param.id).exec();
         return {code: 200, data: res, message: 'success'}
     }
 }
